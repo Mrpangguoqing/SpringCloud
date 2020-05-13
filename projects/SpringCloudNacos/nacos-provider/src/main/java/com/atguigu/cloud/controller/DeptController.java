0e97adce2c2b8572ed9baa22416f69c03554e715
庞国庆 <35087969+Mrpangguoqing@users.noreply.github.com>
2020-05-13 15:19:38 +0800

    commit

--- projects/SpringCloudNacos/nacos-provider/src/main/java/com/atguigu/cloud/controller/DeptController.java
+++ projects/SpringCloudNacos/nacos-provider/src/main/java/com/atguigu/cloud/controller/DeptController.java
@@ -1,12 +1,27 @@
 package com.atguigu.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class DeptController {
+
+    @Value("${basic.url}")
+    private String url;
+
+    @Value("${basic.port}")
+    private int port;
+
+    @Value("${basic.username}")
+    private String userName;
+
+    @Value("${basic.password}")
+    private String password;
 
     @RequestMapping("/hello")
     public String hello(String name){
